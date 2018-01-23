d94999323b85d4934b65c5a779588ebf6b362e64
Alexander Frankel <alexxander.frankel@gmail.com>
2018-01-23 16:09:51 -0500

    Add row_index and col_index to Cell

--- cell.rb
+++ cell.rb
@@ -1,12 +1,23 @@
 class Cell
 
-  attr_reader :live
+  attr_reader :row_index
+  attr_reader :col_index
 
-  def initialize
+  def initialize(row_index, col_index)
     @live = false
+    @row_index = row_index
+    @col_index = col_index
+  end
+
+  def alive?
+    @live
   end
 
   def alive!
     @live = true
+  end
+
+  def dead!
+    @live = false
   end
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -3,12 +3,42 @@ require './cell'
 describe Cell do
 
   subject do
-    Cell.new
+    Cell.new(1, 1)
   end
 
   describe '#initialize' do
     it 'is dead' do
-      expect(subject.live).to equal(false)
+      expect(subject.alive?).to equal(false)
+    end
+
+    it 'has correct row index value' do
+      expect(subject.row_index).to equal(1)
+    end
+
+    it 'has correct col index value' do
+      expect(subject.col_index).to equal(1)
+    end
+  end
+
+  describe '#alive?' do
+    context 'alive' do
+      before do
+        subject.alive!
+      end
+
+      it 'returns true' do
+        expect(subject.alive?).to equal(true)
+      end
+    end
+
+    context 'dead' do
+      before do
+        subject.dead!
+      end
+
+      it 'returns false' do
+        expect(subject.alive?).to equal(false)
+      end
     end
   end
 
@@ -18,7 +48,17 @@ describe Cell do
     end
 
     it 'is alive' do
-      expect(subject.live).to equal(true)
+      expect(subject.alive?).to equal(true)
+    end
+  end
+
+  describe '#dead!' do
+    before do
+      subject.dead!
+    end
+
+    it 'is dead' do
+      expect(subject.alive?).to equal(false)
     end
   end
 
