db320d4eb2ed7af6653e7a98c440f6fbf3ddc4bf
Alexander Frankel <alexxander.frankel@gmail.com>
2018-01-23 16:09:36 -0500

    Add failsafe method to retrieve cell of board

--- board.rb
+++ board.rb
@@ -4,16 +4,38 @@ class Board
 
   attr_reader :rows
 
-  def initialize(height:, width:)
+  def initialize(seed=[])
+    @seed = seed
     @rows = []
+    construct!
+  end
 
-    height.times do
+  def get_cell(row, col)
+    if row < 0 || col < 0
+      nil
+    else
+      @rows.dig(row, col)
+    end
+  end
+
+  private
+
+  def construct!
+    highest_seed_row.times do |row_val|
       row = []
-      width.times do
-        row << Cell.new
+      highest_seed_col.times do |col_val|
+        row << Cell.new(row_val, col_val)
       end
       @rows << row
     end
+  end
+
+  def highest_seed_row
+    @seed.max_by(&:first).first
+  end
+
+  def highest_seed_col
+    @seed.max_by(&:last).last
   end
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,24 +2,60 @@ require './board'
 
 describe Board do
 
-  context 'height and width not provided' do
-    it 'raises an error' do
-      expect{Board.new}.to raise_error(ArgumentError)
+  let!(:seed) {
+    [
+      [1, 3],
+      [7, 7],
+      [12, 10],
+      [5, 2],
+      [10, 15]
+    ]
+  }
+
+  subject do
+    Board.new(seed)
+  end
+
+  describe'#initialize' do
+    it 'has the same number of rows as the highest seed row' do
+      expect(subject.rows.length).to equal(12)
+    end
+
+    it 'has the same number of cols as the highest seed col' do
+      subject.rows.each do |row|
+        expect(row.length).to equal(15)
+      end
     end
   end
 
-  context 'height and width provided' do
-    subject do
-      Board.new(height: 20, width: 20)
+  describe '#get_cell' do
+    context 'row is less than 0' do
+      it 'returns nil' do
+        expect(subject.get_cell(-1, 0)).to be_nil
+      end
     end
 
-    it 'has the correct number of rows' do
-      expect(subject.rows.length).to equal(20)
+    context 'col is less than 0' do
+      it 'returns nil' do
+        expect(subject.get_cell(0, -1)).to be_nil
+      end
     end
 
-    it 'has the correct number of colums' do
-      subject.rows.each do |row|
-        expect(row.length).to equal(20)
+    context 'row does not exist' do
+      it 'returns nil' do
+        expect(subject.get_cell(1000, 0)).to be_nil
+      end
+    end
+
+    context 'col does not exist' do
+      it 'returns nil' do
+        expect(subject.get_cell(0, 1000)).to be_nil
+      end
+    end
+
+    context 'row and col exist' do
+      it 'returns cell' do
+        expect(subject.get_cell(0, 0)).to be_instance_of(Cell)
       end
     end
   end
