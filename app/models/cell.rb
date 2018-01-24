6d73c8303f1bcabe4dac6f693421f5ead837a6ec
Alexander Frankel <alexxander.frankel@gmail.com>
2018-01-24 16:54:37 -0500

    Refactor cell model

--- app/models/cell.rb
+++ app/models/cell.rb
@@ -1,16 +1,9 @@
 class Cell
 
-  def initialize(coordinates, live=false)
-    @live = live
-    @coordinates = validate_coordinates(coordinates)
-  end
-
-  def row_index
-    @coordinates[0]
-  end
-
-  def col_index
-    @coordinates[1]
+  def initialize(row_index, col_index)
+    @row_index = row_index
+    @col_index = col_index
+    @live = false
   end
 
   def alive?
@@ -23,25 +16,6 @@ class Cell
 
   def dead!
     @live = false
-  end
-
-  private
-
-  def validate_coordinates(coordinates)
-    raise_argument_error(coordinates) unless valid_coordinates?(coordinates)
-    coordinates
-  end
-
-  def valid_coordinates?(coordinates)
-    if coordinates[0].nil? || coordinates[1].nil?
-      false
-    else
-      coordinates[0] >= 0 && coordinates[1] >= 0
-    end
-  end
-
-  def raise_argument_error(coordinates)
-    raise ArgumentError, "Cell coordinates #{coordinates} are invalid!"
   end
 
 end
