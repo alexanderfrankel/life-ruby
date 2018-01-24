0b7e0b7f15dffdbe4afc065aee7dbf3d01c6070b
Alexander Frankel <alexxander.frankel@gmail.com>
2018-01-24 14:57:29 -0500

    Add validation to coordinates for a cell

--- app/models/cell.rb
+++ app/models/cell.rb
@@ -1,12 +1,16 @@
 class Cell
 
-  attr_reader :row_index
-  attr_reader :col_index
+  def initialize(coordinates, live=false)
+    @live = live
+    @coordinates = validate_coordinates(coordinates)
+  end
 
-  def initialize(row_index, col_index)
-    @live = false
-    @row_index = row_index
-    @col_index = col_index
+  def row_index
+    @coordinates[0]
+  end
+
+  def col_index
+    @coordinates[1]
   end
 
   def alive?
@@ -20,4 +24,24 @@ class Cell
   def dead!
     @live = false
   end
+
+  private
+
+  def validate_coordinates(coordinates)
+    raise_argument_error(coordinates) unless valid_coordinates?(coordinates)
+    coordinates
+  end
+
+  def valid_coordinates?(coordinates)
+    if coordinates[0].nil? || coordinates[1].nil?
+      false
+    else
+      coordinates[0] >= 0 && coordinates[1] >= 0
+    end
+  end
+
+  def raise_argument_error(coordinates)
+    raise ArgumentError, "Cell coordinates #{coordinates} are invalid!"
+  end
+
 end
--- spec/models/cell_spec.rb
+++ spec/models/cell_spec.rb
@@ -3,19 +3,65 @@ require './app/models/cell'
 describe Cell do
 
   subject do
-    Cell.new(1, 1)
+    Cell.new([1, 1])
   end
 
   describe '#initialize' do
-    it 'is dead' do
-      expect(subject.alive?).to equal(false)
+    context 'default' do
+      it 'is dead by default' do
+        expect(subject.alive?).to equal(false)
+      end
     end
 
-    it 'has correct row index value' do
+    context 'live cell' do
+      subject do
+        Cell.new([1, 1], true)
+      end
+
+      it 'is alive' do
+        expect(subject.alive?).to equal(true)
+      end
+    end
+
+    context 'invalid coordinates' do
+      subject do
+        Cell.new([-1, 1])
+      end
+
+      it 'raises an error' do
+        expect{subject}.to raise_error(ArgumentError)
+      end
+    end
+
+    context 'coordinates are incomplete' do
+      subject do
+        Cell.new([1])
+      end
+
+      it 'raises an error' do
+        expect{subject}.to raise_error(ArgumentError)
+      end
+    end
+
+    context 'coordinates do not exist' do
+      subject do
+        Cell.new([])
+      end
+
+      it 'raises an error' do
+        expect{subject}.to raise_error(ArgumentError)
+      end
+    end
+  end
+
+  describe '#row_index' do
+    it 'is correct value' do
       expect(subject.row_index).to equal(1)
     end
+  end
 
-    it 'has correct col index value' do
+  describe '#col_index' do
+    it 'is correct value' do
       expect(subject.col_index).to equal(1)
     end
   end
